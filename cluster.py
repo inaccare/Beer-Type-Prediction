db8b06ceeef699e354da9d6c3cd4f8e98ecc68e8
Wesley Olmsted <wesleyolmsted@DN51uk20.SUNet>
2018-05-16 23:14:03 -0700

    updated clustering

--- cluster.py
+++ cluster.py
@@ -5,6 +5,8 @@ import pandas as pd
 import numpy as np
 import sys
 
+from sklearn.cluster import KMeans
+
 
 
 def average_vecs(w2v):
@@ -12,6 +14,7 @@ def average_vecs(w2v):
 	with open('beers.csv') as f:
 		df = pd.read_csv(f)
 		styles = df['style'].unique()
+		print(styles)
 		all_styles = np.zeros((89, 100))
 		for i, s in enumerate(styles):
 			all_beers = df.loc[df['style'] == s]
@@ -19,20 +22,26 @@ def average_vecs(w2v):
 			top_thousand = all_beers.head(1000)
 			num_rows = len(top_thousand)
 			for index, row in top_thousand.iterrows():
-				review = row['text'].encode('utf-8').decode('utf-8')
-				review_tokenized = nltk.word_tokenize(review)
+				review = row['text']
+				review_tokenized = nltk.word_tokenize(review.lower())
 				for token in review_tokenized:
-					print(token)
-					summed += w2v[token]
+					if token in w2v.wv.vocab:
+						summed += w2v[token]
 			summed /= num_rows
-			print(summed)
 			all_styles[i] = summed
-		print(all_styles)
+		return all_styles
+
+def cluster(all_styles):
+	kmeans = KMeans()
+	kmeans.fit(all_styles)
+	print(kmeans.labels_)
+
 
 def main():
 	w2vFile = sys.argv[1]
 	w2v = gensim.models.word2vec.Word2Vec.load(w2vFile)
-	average_vecs(w2v)
+	all_styles = average_vecs(w2v)
+	cluster(all_styles)
 
 
 
